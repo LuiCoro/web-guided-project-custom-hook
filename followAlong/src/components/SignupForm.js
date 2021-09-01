6e2589665c85f3fcbdf64db069fd9187bb52eaa6
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-01 13:47:56 -0400

    lecture work-in-progress

--- followAlong/src/components/SignupForm.js
+++ followAlong/src/components/SignupForm.js
@@ -6,7 +6,7 @@ import Button from "../theme/Button";
 import useForm from "./../hooks/useForm";
 
 const initState = {
-  firstName: "",
+  firstName: localStorage.getItem("name"),
   lastName: ""
 }
 
