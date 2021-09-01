3c1c77ffc2d3a7674803546529559303641003a8
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-01 12:30:49 -0400

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
 
-      <SelectedPokemon />
+      <SelectedPokemon selectedPokemon={selectedPokemon}/>
 
       <div id="pokeList">
         {pokemen.map((pokemon) => (
