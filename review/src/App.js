7aeb2d2a4335a6f5c5098fe0222485dba83830f7
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-01 13:04:08 -0400

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -8,31 +8,9 @@ import Pokedex from './components/Pokedex';
 import { getAllPokemon, getPokemon } from './services/pokeService';
 
 
-const usePokeState = () => {
-  const [pokemen, setPokemen] = useState([]);
-  const [selectedPokemon, setSelectedPokemon] = useState({});
-
-  useEffect(() => {
-    setPokemen(getAllPokemon());
-  }, []);
-
-  const handlePoke = (id) => {
-    getPokemon(id)
-      .then((pokemon) => {
-        setSelectedPokemon(pokemon);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
-  return [selectedPokemon, pokemen, handlePoke];
-}
-
-
 function App() {
   const [selectedPokemon, pokemen, handlePoke ] = usePokeState();
-  
+
   return (
     <div className="App">
       <SelectedPokemon selectedPokemon={selectedPokemon}/>
---
+++ review/src/hooks/usePokeState.js
@@ -0,0 +1,24 @@
+
+
+const usePokeState = () => {
+    const [pokemen, setPokemen] = useState([]);
+    const [selectedPokemon, setSelectedPokemon] = useState({});
+  
+    useEffect(() => {
+      setPokemen(getAllPokemon());
+    }, []);
+  
+    const handlePoke = (id) => {
+      getPokemon(id)
+        .then((pokemon) => {
+          setSelectedPokemon(pokemon);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+  
+    return [selectedPokemon, pokemen, handlePoke];
+}
+
+export default usePokeState;
